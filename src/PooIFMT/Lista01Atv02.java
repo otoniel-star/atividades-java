7566413ecc08ace722ec838d53d12c7ab59648fe
otoniel serafim <otoniel.serafimsilva@gmail.com>
2026-04-04 18:48:14 -0400

    feat: adcionando a segunda  atividade da lista 01 de poo ifmt e colocando material de apoio imes e lista de atvidades

--- src/PooIFMT/Lista01Atv02.java
+++ src/PooIFMT/Lista01Atv02.java
@@ -1,4 +1,30 @@
 package PooIFMT;
 
+import java.util.Scanner;
+
+/**
+* 2. Crie um programa que solicite ao usuário um número inteiro  ok
+ * e um intervalo (início e fim).ok
+ *>>>>O programa deve gerar e exibir a tabuada completa
+do número fornecido dentro do intervalo especificado, formatando a saída
+de forma clara com operações de multiplicação.ok
+* */
+
+
 public class Lista01Atv02 {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("digite o numero para a operação: ");
+        int numeroOperacao = leitura.nextInt();
+
+        System.out.println("digite por qual numero ele deve Começar: ");
+        int intervaloInicio = leitura.nextInt();
+        System.out.println("digite por qual numero ele deve Terminar: ");
+        int intervaloFim = leitura.nextInt();
+        System.out.printf("-------------------Tabuada do numero %d :---------------\n",numeroOperacao);
+        for (int i = intervaloInicio; i <= intervaloFim; i++) {
+            System.out.printf(" %d x %d = %d \n", numeroOperacao, i, numeroOperacao*i);
+        }
+    }
 }
